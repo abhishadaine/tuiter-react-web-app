1ce1745294158d9d635a826d81172039873845db
abhishadaine <daine.a@northeastern.edu>
2022-10-14 19:35:16 -0400

    Todo List

--- public/labs/a5/todos/index.js
+++ public/labs/a5/todos/index.js
@@ -1,1 +1,11 @@
-alert('Hello World!');
+
+// alert('Hello World!');
+/* eslint-env jquery */
+import TodoList from "./TodoList.js";
+
+$('#wd-todo').append(`
+   <div class="container">
+       <h1>Todo example</h1>
+       ${TodoList()}
+   </div>
+`);
