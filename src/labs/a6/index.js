804bae01b0c71d72f21449c50a78c7996c7445b9
abhishadaine <daine.a@northeastern.edu>
2022-10-23 11:14:29 -0400

    Lab complete

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -2,11 +2,13 @@ import Classes from "./classes";
 import Styles from "./styles";
 import ConditionalOutput
     from "./conditional-output";
+import TodoItem from "./todo/todo-item";
 
 function Assignment6() {
     return (
         <div>
             <h1>Assignment 6</h1>
+            <TodoItem/>
             <ConditionalOutput/>
             <Styles/>
             <Classes/>
--- src/labs/index.js
+++ src/labs/index.js
@@ -1,6 +1,6 @@
 import Assignment6 from "./a6";
 import Nav from "../nav";
-
+import TodoList from "./a6/todo/todo-list";
 
 
 function Labs() {
@@ -8,6 +8,7 @@ function Labs() {
         <div>
             <Nav/>
             <Assignment6/>
+            <TodoList/>
         </div>
     );
 }
