a21139a9d0b94184f5d40a8199862563b0ae18cc
abhishadaine <daine.a@northeastern.edu>
2022-11-05 18:25:43 -0400

    2.2

--- src/App.js
+++ src/App.js
@@ -30,6 +30,7 @@ import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
 import {BrowserRouter} from "react-router-dom";
 import {Routes, Route} from "react-router";
+import Assignment7 from "./labs/a7";
 
 function App() {
   return (
@@ -41,6 +42,7 @@ function App() {
                   <Route path="/hello"
                          element={<HelloWorld/>}/>
                   <Route path="/tuiter/*" element={<Tuiter/>}/>
+                  <Route path="/*" element={<Assignment7/>}/>
 
               </Routes>
 
