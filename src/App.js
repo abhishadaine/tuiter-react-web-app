5e1c8dd487c12900f19834cb1b0347faa6fc2582
abhishadaine <daine.a@northeastern.edu>
2022-11-06 01:56:16 -0400

    2.6

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
                   <Route path="/hello"
                          element={<HelloWorld/>}/>
                   <Route path="/tuiter/*" element={<Tuiter/>}/>
-                  
+
 
               </Routes>
 
